d9267a1e183ade46384b80d4326bcedaffe536ef
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-02-16 10:04:01 -0300

    Update CV-Online

--- landingpage/assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-async function fetchProfileData() {
-    const url = 'https://web.dio.me/users/jonathasrosa85?tab=skills';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
-}
